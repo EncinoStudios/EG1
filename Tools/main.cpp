07da2fc47954352012154961a6a096576d950f88
Aiden-Wright <aiden.wright@mymail.champlain.edu>
2024-01-30 16:01:03 -0800

    Preparing to add JSON parser

---
+++ Tools/main.cpp
@@ -0,0 +1,72 @@
+#include "GL/glew.h"
+#include "GLFW/glfw3.h"
+
+#include <stdio.h>
+
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+
+#include "imgui/imgui.h"
+#include "imgui/imgui_impl_glfw.h"
+#include "imgui/imgui_impl_opengl3.h"
+
+float lastFrameTime;
+float deltaTime;
+
+int SCREEN_WIDTH = 1080;
+int SCREEN_HEIGHT = 720;
+
+double prevMouseX;
+double prevMouseY;
+bool firstMouseInput = false;
+
+bool wireFrame = false;
+
+int main() {
+	if (!glfwInit()) {
+		printf("glfw failed to init");
+		return 1;
+	}
+
+	GLFWwindow* window = glfwCreateWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "GameDev", 0, 0);
+	glfwMakeContextCurrent(window);
+
+	if (glewInit() != GLEW_OK) {
+		printf("glew failed to init");
+		return 1;
+	}
+
+	// Setup UI Platform/Renderer backends
+	IMGUI_CHECKVERSION();
+	ImGui::CreateContext();
+	ImGui_ImplGlfw_InitForOpenGL(window, true);
+	ImGui_ImplOpenGL3_Init();
+
+	//Dark UI theme.
+	ImGui::StyleColorsDark();
+
+	while (!glfwWindowShouldClose(window)) {
+		ImGui_ImplOpenGL3_NewFrame();
+		ImGui_ImplGlfw_NewFrame();
+		ImGui::NewFrame();
+
+		float time = (float)glfwGetTime();
+		deltaTime = time - lastFrameTime;
+		lastFrameTime = time;
+
+		
+		//Draw UI
+		ImGui::Begin("Settings");
+
+		ImGui::End();
+
+		ImGui::Render();
+		ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+		glfwPollEvents();
+
+		glfwSwapBuffers(window);
+	}
+
+	glfwTerminate();
+	return 0;
+}
